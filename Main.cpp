a56987cfea90c0fb11c26503853b4166f7981725
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2022-10-28 12:57:31 +0200

    Add depth

--- Main.cpp
+++ Main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include <unistd.h>
 
-void foo() {
-  std::cout << "ENTER Foo" << std::endl;
-  sleep(3);
-  std::cout << "EXIT Foo" << std::endl;
-  return;
+void foo(int);
+void bar(int);
+void buz(int);
+
+void foo(int depth) {
+  if (depth > 0) {
+    std::cout << "ENTER Foo" << std::endl;
+    sleep(1);
+    bar(depth - 1);
+    std::cout << "EXIT Foo" << std::endl;
+    return;
+  }
 }
 
-void bar() {
-  std::cout << "ENTER Bar" << std::endl;
-  sleep(4);
-  std::cout << "EXIT Bar" << std::endl;
-  return;
+void bar(int depth) {
+  if (depth > 0) {
+    std::cout << "ENTER Bar" << std::endl;
+    sleep(2);
+    buz(depth - 1);
+    std::cout << "EXIT Bar" << std::endl;
+    return;
+  }
 }
 
-void buz() {
-  std::cout << "ENTER Buz" << std::endl;
-  sleep(5);
-  std::cout << "EXIT Buz" << std::endl;
-  return;
+void buz(int depth) {
+  if (depth > 0) {
+    std::cout << "ENTER Buz" << std::endl;
+    sleep(3);
+    foo(depth - 1);
+    std::cout << "EXIT Buz" << std::endl;
+    return;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -29,15 +42,15 @@ int main(int argc, char *argv[]) {
   bool Buz __attribute__((feature_variable("Buz"))) = argc > 3;
 
   if (Foo) {
-    foo();
+    foo(argc);
   }
 
   if (Bar) {
-    bar();
+    bar(argc);
   }
 
   if (Buz) {
-    buz();
+    buz(argc);
   }
 
   return 0;
