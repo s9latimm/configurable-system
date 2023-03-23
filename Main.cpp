a4ccb919b9cab2f1caa615dfbd04ff24bddcad84
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2023-03-23 22:37:55 +0100

    Add macro

--- Main.cpp
+++ Main.cpp
@@ -1,57 +1,23 @@
+#include <chrono>
 #include <iostream>
-#include <unistd.h>
+#include <thread>
 
-void foo(int);
-void bar(int);
-void buz(int);
-
-void foo(int depth) {
-  if (depth > 0) {
-    std::cout << "ENTER Foo" << std::endl;
-    sleep(1);
-    bar(depth - 1);
-    std::cout << "EXIT Foo" << std::endl;
-    return;
+#define FEATURE(name)                                                          \
+  void f_##name() {                                                            \
+    std::cout << "ENTER " << #name << std::endl;                               \
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));               \
+    std::cout << "EXIT " << #name << std::endl;                                \
   }
-}
-
-void bar(int depth) {
-  if (depth > 0) {
-    std::cout << "ENTER Bar" << std::endl;
-    sleep(2);
-    buz(depth - 1);
-    std::cout << "EXIT Bar" << std::endl;
-    return;
-  }
-}
-
-void buz(int depth) {
-  if (depth > 0) {
-    std::cout << "ENTER Buz" << std::endl;
-    sleep(3);
-    foo(depth - 1);
-    std::cout << "EXIT Buz" << std::endl;
-    return;
-  }
-}
+#include "Features.def"
 
 int main(int argc, char *argv[]) {
 
-  bool Foo __attribute__((feature_variable("Foo"))) = argc > 1;
-  bool Bar __attribute__((feature_variable("Bar"))) = argc > 2;
-  bool Buz __attribute__((feature_variable("Buz"))) = argc > 3;
-
-  if (Foo) {
-    foo(argc);
+#define FEATURE(name)                                                          \
+  bool F_##name __attribute__((feature_variable(#name))) = argc > 0;           \
+  if (F_##name) {                                                              \
+    f_##name();                                                                \
   }
-
-  if (Bar) {
-    bar(argc);
-  }
-
-  if (Buz) {
-    buz(argc);
-  }
+#include "Features.def"
 
   return 0;
 }
