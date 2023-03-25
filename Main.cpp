63f520bca7a48e58219e5ae4bf12dfd22ab21ac0
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2023-03-25 15:12:07 +0100

    Add workloads

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,78 @@
+#include <algorithm>
+#include <iostream>
 #include <time.h>
+#include <vector>
 
-void sleep(int) __attribute__((xray_never_instrument));
-void sleep(int milliseconds) {
-  struct timespec Rem;
-  struct timespec Req = {(int)(milliseconds / 1000),
-                         (milliseconds % 1000) * 1000000};
-  nanosleep(&Req, &Rem);
-}
+struct Workload {
+  static void sleep(unsigned Milliseconds)
+      __attribute__((xray_never_instrument)) {
+    struct timespec Time = {Milliseconds / 1'000,
+                            (Milliseconds % 1'000) * 1'000'000};
+    nanosleep(&Time, nullptr);
+  }
 
-#define FEATURE(name, iter)                                                    \
-  void f_##name() __attribute__((xray_always_instrument));                     \
-  void f_##name() { sleep(1); }
+  static void busy() __attribute__((xray_never_instrument)) {
+    for (unsigned Iter = 0; Iter < 10'000; ++Iter) {
+      asm volatile("" : "+g"(Iter) : :);
+    }
+  }
+
+  static void collatz() __attribute__((xray_never_instrument)) {
+    for (unsigned Number = std::rand(); Number > 1;
+         Number = Number % 2 ? 3 * Number + 1 : Number >> 1) {
+      asm volatile("nop");
+    }
+  }
+
+  static void sort() __attribute__((xray_never_instrument)) {
+    std::vector<unsigned> Values;
+    while (Values.size() < 42) {
+      Values.push_back(std::rand());
+      for (unsigned Iter = 1; Iter < Values.size(); ++Iter) {
+        unsigned Value = Values[Iter];
+        unsigned Index = Iter;
+        for (; Index > 0 && Values[Index - 1] > Value; --Index) {
+          Values[Index] = Values[Index - 1];
+        }
+        Values[Index] = Value;
+      }
+      std::reverse(Values.begin(), Values.end());
+      asm volatile("nop");
+    }
+  }
+
+  static void run(unsigned Milliseconds, void (*Workload)())
+      __attribute__((xray_never_instrument)) {
+    struct timespec Time;
+    timespec_get(&Time, TIME_UTC);
+    unsigned Start = Time.tv_sec * 1'000 + Time.tv_nsec / 1'000'000;
+    for (unsigned End = Start + Milliseconds; Start < End;
+         Start = Time.tv_sec * 1'000 + Time.tv_nsec / 1'000'000) {
+      Workload();
+      timespec_get(&Time, TIME_UTC);
+    }
+  }
+
+  static void run(unsigned Milliseconds, void (*Workload)(unsigned))
+      __attribute__((xray_never_instrument)) {
+    Workload(Milliseconds);
+  }
+};
+
+#define FEATURE(name, iter, workload)                                          \
+  void f_##name() __attribute__((xray_always_instrument)) {                    \
+    Workload::run(WORKLOAD, &Workload::workload);                              \
+  }
 #include "Features.def"
 
 int main(int argc, char *argv[]) {
 
-#define FEATURE(name, iter)                                                    \
+  std::srand(42);
+
+#define FEATURE(name, iter, workload)                                          \
   bool F_##name __attribute__((feature_variable(#name))) = argc > 0;           \
+  std::cout << "FEATURE(" << #name << ", " << #iter << ", " << #workload       \
+            << ")" << std::endl;                                               \
   for (unsigned Iter = 0; Iter < iter; ++Iter) {                               \
     if (F_##name) {                                                            \
       f_##name();                                                              \
